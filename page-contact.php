93d0a539e3ac82b1b27bba27e10e4bde82115ed8
Colin Caufield <cjcaufield@gmail.com>
2015-09-16 22:50:23 +0000

    Front page and Contact page tweaks.

--- page-contact.php
+++ page-contact.php
@@ -19,32 +19,41 @@ get_header(); ?>
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12">
+			<!-- <div class="col-xs-12 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2"> -->
 				<div class="text-center">
-					<h2><a href="">Email us</a> for support.</h2>
-					<h3>FAQs:  <a href="">Cathode</a>, <a href="">Blinky</a></h3>
-					<div class="spacer50"></div>
-					<h2>We're also on <a href="">Twitter</a> and <a href="">Facebook</a>.</h2>
-					<h3>Pages: <a href="">Cathode</a>, <a href="">Blinky</a></h3>
-					<div class="spacer50"></div>
-					<h2>Code is hosted on <a href="">Github</a>.</h2>
 					<!--
-					<h3>Social</h3>
-					<p>Twitter:<br />
-					<a href="http://www.twitter.com/secretgeo">@secretgeo</a></p>
-					<p>Facebook:<br />
-					<a href="http://www.facebook.com/pages/Secret-Geometry/232627286800703">Secret Geometry</a><br />
-					<a href="http://www.facebook.com/CathodeApp">Cathode</a><br />
-					<a href="http://www.facebook.com/BlinkyTextEditor">Blinky</a></p>
-					<h3>Email</h3>
-					<p>Support:<br />
-					<a href="mailto:support@secretgeometry.com">support@secretgeometry.com</a></p>
-					<p>Press inquiries:<br />
-					<a href="mailto:pr@secretgeometry.com">pr@secretgeometry.com</a></p>
-					<p>Art projects:<br />
-					<a href="mailto:creative@secretgeometry.com">creative@secretgeometry.com</a></p>
-					<h3>FAQs</h3>
-					<p><a href="/apps/cathode/faq">Cathode</a><br />
-					<a href="/apps/blinky/faq">Blinky</a></p>
+					<div class="feature-text">
+						<div class="feature-content">
+					-->
+							<h1><a href="">Email us</a> for support.</h1>
+							<h3>FAQs:  <a href="">Cathode</a>, <a href="">Blinky</a></h3>
+							<div class="spacer50"></div>
+							<h2>We're also on <a href="">Twitter</a> and <a href="">Facebook</a>.</h2>
+							<h3>Pages: <a href="">Cathode</a>, <a href="">Blinky</a></h3>
+							<div class="spacer50"></div>
+							<h2>Public code is on <a href="">Github</a>.</h2>
+							<!--
+							<h3>Social</h3>
+							<p>Twitter:<br />
+							<a href="http://www.twitter.com/secretgeo">@secretgeo</a></p>
+							<p>Facebook:<br />
+							<a href="http://www.facebook.com/pages/Secret-Geometry/232627286800703">Secret Geometry</a><br />
+							<a href="http://www.facebook.com/CathodeApp">Cathode</a><br />
+							<a href="http://www.facebook.com/BlinkyTextEditor">Blinky</a></p>
+							<h3>Email</h3>
+							<p>Support:<br />
+							<a href="mailto:support@secretgeometry.com">support@secretgeometry.com</a></p>
+							<p>Press inquiries:<br />
+							<a href="mailto:pr@secretgeometry.com">pr@secretgeometry.com</a></p>
+							<p>Art projects:<br />
+							<a href="mailto:creative@secretgeometry.com">creative@secretgeometry.com</a></p>
+							<h3>FAQs</h3>
+							<p><a href="/apps/cathode/faq">Cathode</a><br />
+							<a href="/apps/blinky/faq">Blinky</a></p>
+							-->
+					<!--
+						</div>
+					</div>
 					-->
 				</div>
 			</div>
