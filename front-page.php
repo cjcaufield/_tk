68ddaeef138d81b74fa261476974b749c10f9f68
Colin Caufield <cjcaufield@gmail.com>
2015-09-16 08:47:47 +0000

    Front page logo layout fixes.

--- front-page.php
+++ front-page.php
@@ -14,24 +14,41 @@ get_header(); ?>
 
 </div><!-- end outer container -->
 
-<div class="jumbotron tight-jumbotron home-page-image-jumbotron">
-    <div class="text-center">
-        <a href="/apps/cathode">
-			<img src="/wp-content/uploads/squattersreversed.jpg" width="100%" border="0" />
-		</a>
-    </div>
+<div class="jumbotron tight-jumbotron jumbotron-white home-page-image-jumbotron">
+	<div class="text-center">
+		<div class="home-page-image-div">
+	        <a href="/apps/cathode">
+				<img src="/wp-content/uploads/squattersreversed.jpg" width="100%" border="0" />
+			</a>
+			<!--
+			<div class="home-page-image-overlay">
+				<h1>Secret Geometry</h1>
+			</div>
+			-->
+		</div>
+	</div>
 </div>
 <div class="jumbotron big-vertical-padding jumbotron-white home-page-logo-jumbotron">
-	<div class="text-center">
+	<div class="home-page-logo-outer-div">
 		<div class="home-page-logo-div">
-			<div class="sgbiglogo"></div>
-			<h1>Some really big text.</h1>
+			<div class="text-center">
+				<div class="sgbiglogo"></div>
+				<h1>Secret Geometry</h1>
+			</div>
+			<div class="spacer30"></div>
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-12 col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2">
+						<p style="text-align:justify">A circle is a simple <a title="Shape" href="https://en.wikipedia.org/wiki/Shape">shape</a> in <a title="Euclidean geometry" href="https://en.wikipedia.org/wiki/Euclidean_geometry">Euclidean geometry</a>. It is the set of all <a title="Point (geometry)" href="https://en.wikipedia.org/wiki/Point_(geometry)">points</a> in a <a class="mw-redirect" title="Plane (mathematics)" href="https://en.wikipedia.org/wiki/Plane_(mathematics)">plane</a> that are at a given distance from a given point, the <a title="Centre (geometry)" href="https://en.wikipedia.org/wiki/Centre_(geometry)">centre</a>; equivalently it is the curve traced out by a point that moves so that its distance from a given point is constant. The distance between any of the points and the centre is called the <a title="Radius" href="https://en.wikipedia.org/wiki/Radius">radius</a>.</p>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 </div>
 <div class="jumbotron big-vertical-padding">
 	<div class="text-center">
-		<h2>Some more slightly smaller text.</h2>
+		<h2>Some slightly smaller text.</h2>
 	</div>
 </div>
 <div class="container"><!-- begin new outer container -->
--- page-contact.php
+++ page-contact.php
@@ -18,28 +18,35 @@ get_header(); ?>
 <div class="jumbotron contact-jumbotron">
 	<div class="container">
 		<div class="row">
-			<div class="col-md-4">
-				<h3>Social</h3>
-				<p>Twitter:<br />
-				<a href="http://www.twitter.com/secretgeo">@secretgeo</a></p>
-				<p>Facebook:<br />
-				<a href="http://www.facebook.com/pages/Secret-Geometry/232627286800703">Secret Geometry</a><br />
-				<a href="http://www.facebook.com/CathodeApp">Cathode</a><br />
-				<a href="http://www.facebook.com/BlinkyTextEditor">Blinky</a></p>
-			</div>
-			<div class="col-md-4">
-				<h3>Email</h3>
-				<p>Support:<br />
-				<a href="mailto:support@secretgeometry.com">support@secretgeometry.com</a></p>
-				<p>Press inquiries:<br />
-				<a href="mailto:pr@secretgeometry.com">pr@secretgeometry.com</a></p>
-				<p>Art projects:<br />
-				<a href="mailto:creative@secretgeometry.com">creative@secretgeometry.com</a></p>
-			</div>
-			<div class="col-md-4">
-				<h3>FAQs</h3>
-				<p><a href="/apps/cathode/faq">Cathode</a><br />
-				<a href="/apps/blinky/faq">Blinky</a></p>
+			<div class="col-md-12">
+				<div class="text-center">
+					<h2><a href="">Email us</a> for support.</h2>
+					<h3>FAQs:  <a href="">Cathode</a>, <a href="">Blinky</a></h3>
+					<div class="spacer50"></div>
+					<h2>We're also on <a href="">Twitter</a> and <a href="">Facebook</a>.</h2>
+					<h3>Pages: <a href="">Cathode</a>, <a href="">Blinky</a></h3>
+					<div class="spacer50"></div>
+					<h2>Code is hosted on <a href="">Github</a>.</h2>
+					<!--
+					<h3>Social</h3>
+					<p>Twitter:<br />
+					<a href="http://www.twitter.com/secretgeo">@secretgeo</a></p>
+					<p>Facebook:<br />
+					<a href="http://www.facebook.com/pages/Secret-Geometry/232627286800703">Secret Geometry</a><br />
+					<a href="http://www.facebook.com/CathodeApp">Cathode</a><br />
+					<a href="http://www.facebook.com/BlinkyTextEditor">Blinky</a></p>
+					<h3>Email</h3>
+					<p>Support:<br />
+					<a href="mailto:support@secretgeometry.com">support@secretgeometry.com</a></p>
+					<p>Press inquiries:<br />
+					<a href="mailto:pr@secretgeometry.com">pr@secretgeometry.com</a></p>
+					<p>Art projects:<br />
+					<a href="mailto:creative@secretgeometry.com">creative@secretgeometry.com</a></p>
+					<h3>FAQs</h3>
+					<p><a href="/apps/cathode/faq">Cathode</a><br />
+					<a href="/apps/blinky/faq">Blinky</a></p>
+					-->
+				</div>
 			</div>
 		</div>
 	</div><!-- .entry-content -->
